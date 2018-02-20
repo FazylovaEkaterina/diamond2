f38cc0a194424dc20146b27f229db13238996846
FazylovaEkaterina <reebad23@gmail.com>
2018-02-20 14:13:04 +0300

    added VS2015 PROJECT

---
+++ DIAMOND2/DIAMOND/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Painter
+{
+    
+
+    public Painter() { }
+    public char ch;
+    public bool Read(string str)
+    {
+        char s;
+        Console.Write("Enter the letter: ");
+
+
+        try
+        {
+            s = Convert.ToChar(str);
+
+            if (((int)s < (int)('A')) | ((int)s > (int)('Z')))
+            {
+               // Console.WriteLine("You entered an incorrect character!");
+
+                return false;
+
+            }
+        }
+        catch (System.FormatException e)
+        {
+           // Console.WriteLine("You entered a string, not a character!" + e.Message);
+            return false;
+
+
+        }
+        this.ch = s;
+        return true;
+    }
+    public void Draw()
+    {
+        char c;
+        int j, i, n, d, f, z, p;
+
+
+
+        c = 'A';
+        n = (int)this.ch - (int)c;
+        j = n;
+        p = n;
+        n = n + n;
+
+        for (i = 0; i <= n; i++) //strat drawing a diamond 
+        {
+            for (d = 1; d <= j; d++) // draw spaces 
+            {
+                Console.Write("_");
+            }
+
+            Console.Write(c);
+            if (c != 'A') // draw spaces between letters 
+            {
+                z = ((int)c - (int)'A') * 2 - 1;
+                for (f = 1; f <= z; f++)
+                {
+                    Console.Write("_");
+                }
+                Console.Write(c);
+            }
+            for (d = 1; d <= j; d++) // draw spaces 
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine();
+
+            if (i < p) // here we check that we draw first half of diamond 
+            { // and need to draw a second half of diamond 
+                j--;
+                c = (char)((int)c + 1);
+            }
+            else
+            {
+                j++;
+                c = (char)((int)c - 1);
+            }
+        }
+        Console.ReadKey();
+
+    }
+}
+
+namespace DIAMOND
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Painter letter = new Painter();
+            if (letter.Read(Console.ReadLine()))
+                letter.Draw();
+            
+
+
+        }
+    }
+}
---
+++ DIAMOND2/UnitTestProject1/UnitTestprogram.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestprogram
+    {
+        [TestMethod]
+        public void input_s_in_correct_type_of_character()
+        {
+            // arrange
+            string s = "string not character";
+            bool expected = false;
+            //act
+            Painter paint = new Painter();
+            bool pr = paint.Read(s);
+            //assert
+
+            Assert.AreEqual(expected, pr);
+        }
+
+        [TestMethod]
+        public void input_s_in_inccorrect_character_1()
+        {
+            // arrange
+            string s = "s";
+            bool expected = false;
+            //act
+            Painter paint = new Painter();
+            bool pr = paint.Read(s);
+            //assert
+
+            Assert.AreEqual(expected, pr);
+
+        }
+
+        [TestMethod]
+        public void input_s_in_inccorrect_character_2()
+        {
+            // arrange
+            string s = "1";
+            bool expected = false;
+            //act
+            Painter paint = new Painter();
+            bool pr = paint.Read(s);
+            //assert
+
+            Assert.AreEqual(expected, pr);
+
+        }
+
+        [TestMethod]
+        public void input_s_in_inccorrect_character_3()
+        {
+            // arrange
+            string s = "ф";
+            bool expected = false;
+            //act
+            Painter paint = new Painter();
+            bool pr = paint.Read(s);
+            //assert
+
+            Assert.AreEqual(expected, pr);
+
+        }
+
+        [TestMethod]
+        public void input_s_in_correct_character()
+        {
+            // arrange
+            string s = "D";
+            bool expected = true;
+            //act
+            Painter paint = new Painter();
+            bool pr = paint.Read(s);
+            //assert
+
+            Assert.AreEqual(expected, pr);
+
+        }
+    }
+}
